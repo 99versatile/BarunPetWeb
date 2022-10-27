8aaf6bcb6cb21b17d16790f24289ad1e9a72cd71
99versatile <99versatile@MacBookPro.local>
2022-10-27 17:11:40 +0900

    added graph v.1.0.2

--- app.js
+++ app.js
@@ -1,46 +1,56 @@
 const isNormal = document.querySelector('#normality');
 
 const button = document.querySelector('#connect');
-button.onclick = () => {connectBluetooth()};
+var serviceCh = null;
+button.onclick = () => {
+  connectBluetooth();
+  getSensorStatus(serviceCh);
+};
 
 const connectBluetooth = () => {
   navigator.bluetooth.requestDevice({ 
     filters: [{ 
-      services: [0x1234, 0x12345678, '99999999-0000-1000-8000-00805f9b34fb'] // insert bluetooth UUID
-    }] 
+      services: ["19b10000-e8f2-537e-4f6c-d104768a1214"]  // insert bluetooth UUID
+    }]
   })
-  .then(device = device.gatt.connect())
+  .then(device => device.gatt.connect())
   .then(server => {
-    // Getting Binary Sensor Service…
-    return server.getPrimaryService('binary_sensor_service');
+    return server.getPrimaryService("19b10000-e8f2-537e-4f6c-d104768a1214");
   })
   .then(service => {
-    // Getting Sensor Status Characteristic…
-    return service.getCharacteristic('sensor_status');
+    serviceCh = service;
   })
+}
+
+const getSensorStatus = (service) => {
+  service.getCharacteristic("19b10001-e8f2-537e-4f6c-d104768a1214")
   .then(characteristic => {
-    // Reading Sensor Status…
+    // Reading Sensor Status
     return characteristic.readValue();
   })
   .then(value => {
-    console.log(`Sensor Value: ${value.getUint8(0)}`);
+    console.log(value.getuint8(byteoffset)); //for문 돌려서 자릿수에 해당하는 데이터 array에 채워주기
   })
   .catch(error => { console.error(error); });
 }
+
+
+
+
 
 const connectBattery = () => {
   navigator.bluetooth.requestDevice({ filters: [{ services: ['binary_sensor_service'] }] })
   .then(device => device.gatt.connect())
   .then(server => {
-    // Getting Battery Service…
+    // Getting Battery Service
     return server.getPrimaryService('binary_sensor_service');
   })
   .then(service => {
-    // Getting Battery Level Characteristic…
+    // Getting Battery Level Characteristic
     return service.getCharacteristic('sensor_status');
   })
   .then(characteristic => {
-    // Reading Battery Level…
+    // Reading Battery Level
     return characteristic.readValue();
   })
   .then(value => {
